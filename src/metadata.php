c85b944e00fdf31078f009630e569b25d478c00f
Daniel Seifert <ds@shopmodule.com>
2023-09-07 09:06:02 +0200

    adjust version informations

--- src/metadata.php
+++ src/metadata.php
@@ -57,7 +57,7 @@ $aModule = [
         'de'        => 'Einmalpasswort (TOTP) als zweiter Faktor bei der Anmeldung im OXID eSales Shop',
         'en'        => 'One-time password (TOTP) as second factor for login in OXID eSales shop',
     ],
-    'version'       => '2.1.0.0',
+    'version'       => '2.1.1.0',
     'author'        => 'D&sup3; Data Development (Inh.: Thomas Dartsch)',
     'email'         => 'support@shopmodule.com',
     'url'           => 'https://www.oxidmodule.com/',
